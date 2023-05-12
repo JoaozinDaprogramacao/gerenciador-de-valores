a0f7b1bc165cfc24ed325ad714c4e37d65b869d1
João Emanuel <joaoemanueljanauba@gmail.com>
2023-05-12 17:18:41 -0300

    adicionando todos os arquivos do projeto - first push

---
+++ src/main/java/com/gerenciador/gerenciadorDeValores/GerenciadorDeValoresApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciador.gerenciadorDeValores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeValoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeValoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gerenciador/gerenciadordevalores/GerenciadorDeValoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.gerenciadordevalores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeValoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
